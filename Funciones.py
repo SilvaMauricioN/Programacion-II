518119bb665e3c3363f07c3f125ab9b1dd825b61
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2022-12-10 17:31:58 -0300

    elimina modificar pelicula

--- Funciones.py
+++ Funciones.py
@@ -1,42 +1,83 @@
 import json 
+import requests
 
-#Creo diccionario apartir de un json
-# with open ('Peliculas.json', encoding='utf-8') as archivo:
-#     Peliculas = json.load(archivo)
+
 
 #Modo Publico
 
 #funcion para ver las ultimas 10 pelicuas subidas
 def Ultimas_Peliculas():
 
+    Datos = requests.get("http://127.0.0.1:5000/Peliculas")
+    Peliculas = Datos.json()
+    
     print("Ultimas Peliculas")
     print()
-#calulo en que posicion empezar
+    #calulo en que posicion empezar
     indice=len(Peliculas['Movies'])-10
 
     for i in Peliculas['Movies'][indice:]:
+        print()
         print(i['titulo'])
+        print()
+        print(i['sinopsis'])
 
 #funcion para buscar peliculas por nombre de director
 def Buscar_Director():
 
-    director =input("Ingrese Nombre de Director: ")
-    encontrado=False
+    director =input("Ingrese el Nombre del Director: ")
+    Datos = requests.get("http://127.0.0.1:5000/Peliculas/"+director)
+    print (Datos.url)
+    
+    Peliculas= Datos.json()
+    print(director)
 
-    for i in Peliculas['Movies']:
-        if director.upper() == i["director"].upper():
-            print()
-            print("Pelicula Dirigida: ",i["titulo"])
-            print()
-            print("Sinopsis: ",i["sinopsis"])
-            encontrado=True
-    if (encontrado==False):
-        print("No se encontro director",director)
+    if type(Peliculas) == dict:
 
+        for i in Peliculas['peliculas']:
+            print(i)
+    else:
+        print(Peliculas)    
 
+#Modificar pelicula existente
 def modificar_peliculas():
-    #en proceso
-    return None
+    
+    pelicula = input("Ingrese el Nombre de la Pelicula: ")
+
+
+    año = input("Ingrese el año de la pelicula: ")
+    director = input("Ingrese nombre del Director: ")
+    genero = input("Ingrese genero de la pelicula: ")
+    sinopsis = input ("Ingrese la sinopsis: ")
+    img = input("Url de la imagen de la pelicula: ")
+    duracion = input("Ingrese duracion de la pelicula: ")
+    reparto = input("Ingrese reparto de la pelicula: ")
+
+    pelicula_a_modificar ={
+        "titulo":"",
+        'año' :"",
+        'director':"",
+        'genero':"",
+        'sinopsis':"",
+        'img':"",
+        'duracion':"",
+        'reparto':""
+            }
+    pelicula_a_modificar['titulo']=pelicula
+    pelicula_a_modificar["año"]=año
+    pelicula_a_modificar["director"]=director
+    pelicula_a_modificar["genero"]=genero
+    pelicula_a_modificar["sinopsis"]=sinopsis
+    pelicula_a_modificar["img"]=img
+    pelicula_a_modificar["duracion"]=duracion
+    pelicula_a_modificar["reparto"]=reparto
+    
+    Datos = requests.put("http://127.0.0.1:5000/Modificar/"+pelicula, json=pelicula_a_modificar)
+    print(Datos.url)
+    print(pelicula_a_modificar)
+    mensaje=Datos.json()
+    print(mensaje)
+    #print(Datos.text)
 
 # Todos los Menus
 def menu_inicial():
@@ -70,7 +111,7 @@ def ingreso_usuario():
 
     return usuario, contraseña
 
-def comprovar_usuario():
+def comprobar_usuario():
     print("en espera")
 
 #Control de errores y verificación
@@ -88,27 +129,9 @@ def Eliminar_Pelicula():
 
     titulo = input("Nombre de pelicula a eliminar: ")
     #Variable de Control
-    encontrado=False
-
-    with open ('Peliculas.json','r',encoding='utf-8') as archivo:
-        Peliculas = json.load(archivo)
-
-    for i in Peliculas['Movies']:
-
-        if titulo.upper() == i['titulo'].upper():
-            encontrado=True
-            if len(i['criticas']) == 0:
-
-                Peliculas['Movies'].remove(i)
-                print(titulo, 'eliminada del registro')               
-
-                with open ('peliculas.json', 'w', encoding='utf-8')as archivo:
-                    json.dump(Peliculas, archivo, indent=4, ensure_ascii = False)
-
-            else:    
-                print('No se puede eliminar ', titulo,' incluye comentarios')
-                
-    if (encontrado==False):
-        print('La pelicula no existe')
-        print('hola nico')
     
+    Datos = requests.delete("http://127.0.0.1:5000/Eliminar/"+titulo)
+    mensaje=Datos.json()
+    print(mensaje)
+    
+
